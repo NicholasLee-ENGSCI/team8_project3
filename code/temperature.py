b4d4ab81c4d6dd01c92bfcd49b4280a4f2575f20
Shalin Naidoo <shalin.naidoo.personal@gmail.com>
2021-09-12 19:15:16 +1200

    temperature docstring and comments

--- code/temperature.py
+++ code/temperature.py
@@ -6,18 +6,60 @@ import pressure as p
 
 
 def interp(t0, t1, dt):
+    """ Return the interpolated values of pressure for a given range of time.
+
+                Parameters:
+                -----------
+                t0 : float
+                    Initial time.
+                t1 : float
+                    Final time.
+                dt : float
+                    Time step size.
+
+                Returns:
+                --------
+                t : array-like
+                    array of times
+                temp_interp : array-like
+                    array of interpolated temperature values
+
+        """
+
     n = int(np.ceil((t1 - t0) / dt))    # number of steps
     t = t0 + np.arange(n + 1) * dt      # time array
 
+    # read and interpolate temperature values
     time, temp = np.genfromtxt('gr_T.txt', delimiter=',', skip_header=1).T  # temperature (gr_t data)
     temp_interp = np.interp(t, time, temp)
-
-    #f, ax = plt.subplots(1)
-    #ax.plot(t, temp_interp, 'bo', marker='o')
 
     return t, temp_interp
 
 def analytical(t, q, at, bp, T0, Tc):
+    """ Return the pressure value for a given time
+
+        Parameters:
+        -----------
+        t : float
+            Independent variable.
+        q : float
+            total extraction rate.
+        at : float
+            cold water inflow parameter
+        bp : float
+            pressure recharge strength parameter.
+        T0 : float
+            temperature of recharge source.
+        Tc : float
+            temperature of cold water inflow.
+
+        Returns:
+        T : float
+            Pressure value for given time and parameters
+
+        Notes:
+            This function assumes a constant extraction rate therefore the slow drainage term is equal to 0
+    """
 
     return Tc + (T0 - Tc)*math.exp(-1 * (at*q/bp)*(math.exp(-bp*t) + bp*t - 1))
 
@@ -30,24 +72,20 @@ def ode_model(t, temp, pr, a, b, p0, t0, tc):
         -----------
         t : float
             Independent variable.
-        T : float
+        temp : float
             Dependent variable.
-        at : float
-            cold water inflow parameter.
-        bp : float
-            cold water inflow parameter (recharge strength parameter). 
-        ap : float
-            cold water inflow parameter (extraction strength parameter).
-        bt : float
-            conduction strength parameter.
-        P : float
+        pr : float
             pressure.
-        P0 : float
-            initial pressure.
-        Tx : float
-            temperature that depends on direction of flow.
-        T0 : float
+        a : float
+            cold water inflow parameter.
+        b : float
+            conduction parameter (recharge strength parameter).
+        p0 : float
+            pressure at recharge source.
+        t0 : float
             initial value of the dependent variable.
+        tc : float
+            temperature of cold water inflow.
 
         Returns:
         --------
@@ -66,14 +104,12 @@ def ode_model(t, temp, pr, a, b, p0, t0, tc):
         >>> temperature_ode_model(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
         = -3.5
     """
-    # Tx is equal to T if P > P0, otherwise Tx is equal to Tcold.
+    # check for direction of flow
     if pr > p0:
         tempx = temp
     else:
-        tempx = tc  # otherwise Tx is equal to temperature of the cold water injection, 30 degrees celsius.
+        tempx = tc
 
-    # the first derivative returns dT/dt = -at*bp*(1/ap)*(Tx-T)-bt*(T-T0) where all the parameters are provided as
-    # inputs
     return -a * (pr - p0) * (tempx - temp) - b * (temp - t0)
 
 
@@ -84,12 +120,16 @@ def solve_ode(f, t0, t1, dt, x0, pr, pars):
         -----------
         f : callable
             Function that returns dxdt given variable and parameter inputs.
-        t : array-like
-            time of solution.
+        t0 : float
+            Time at solution start.
+        t1 : float
+            Time at solution end.
         dt : float
             Time step length.
         x0 : float
             Initial value of solution.
+        pr : array like
+            fitted pressure values.
         pars : array-like
             List of parameters passed to ODE function f.
 
@@ -104,20 +144,13 @@ def solve_ode(f, t0, t1, dt, x0, pr, pars):
         ------
         ODE should be solved using the Improved Euler Method.
 
-        Function q(t) should be hard coded within this method. Create duplicates of
-        solve_ode for models with different q(t).
-
-        Assume that ODE function f takes the following inputs, in order:
-            1. independent variable
-            2. dependent variable
-            3. forcing term, q
-            4. all other parameters
+        The Temperature LPM is coupled to the pressure using the fitted pressure value
     """
 
-    n = int(np.ceil((t1 - t0) / dt))  # number of steps
-    ts = t0 + np.arange(n + 1) * dt  # time array
-    ys = 0. * ts  # array to store solution values
-    ys[0] = x0  # set initial value of solution array
+    n = int(np.ceil((t1 - t0) / dt))    # number of steps
+    ts = t0 + np.arange(n + 1) * dt     # time array
+    ys = 0. * ts                        # array to store solution values
+    ys[0] = x0                          # set initial value of solution array
         
 
     # calculate solution values using Improved Euler
@@ -131,7 +164,43 @@ def solve_ode(f, t0, t1, dt, x0, pr, pars):
 
 
 def helper(t, dt, x0, pr, a, b, p0, temp0, tempc):
+    """ A helper method for curve_fit.
 
+        Parameters:
+        -----------
+        t : float
+            Independent variable.
+        dt : float
+            Time step length.
+        x0 : float
+            Initial value of the dependent variable.
+        pr : array like
+            fitted pressure values.
+        a : float
+            cold water inflow parameter.
+        b : float
+            conduction parameter (recharge strength parameter).
+        p0 : float
+            hydrostatic pressure of recharge source.
+        temp0 : float
+            temperature of recharge source
+        tempc : float
+            temperature of cold water inflow.
+
+        Returns:
+        --------
+        dxdt : float
+            Derivative of dependent variable with respect to independent variable.
+
+        Notes:
+        ------
+        this method when used in combination with the lambda function allows us to make quick change to our other
+        parameters and resolve for the a, b
+
+        It also breaks up the time array into the correct format.
+
+    """
+    # converting time to correct format
     t0 = t[0]
     t1 = t[-1]
 
@@ -139,8 +208,50 @@ def helper(t, dt, x0, pr, a, b, p0, temp0, tempc):
 
 
 def fit(t, temp, dt, x0, pr, p0, temp0, tempc):
+    """ A helper method for curve_fit.
+
+        Parameters:
+        -----------
+        t : float
+            Independent variable.
+        dt : float
+            Time step length.
+        x0 : float
+            Initial value of the dependent variable.
+        pr : array like
+            fitted pressure values.
+        a : float
+            cold water inflow parameter.
+        b : float
+            conduction parameter (recharge strength parameter).
+        p0 : float
+            hydrostatic pressure of recharge source.
+        temp0 : float
+            temperature of recharge source
+        tempc : float
+            temperature of cold water inflow.
+
+        Returns:
+        --------
+        ap : float
+            Extraction strength parameter.
+        bp : float
+            Recharge strength parameter.
+        cp : float
+            Slow drainage strength parameter.
+
+        Notes:
+        ------
+        The noise of the data is the how the seasons effects the temperature values.
+        The covariance array from curve_fit can be used to model uncertainty
+    """
+    # uncertainty calculation for temperature
     sigma = [2]*len(t)
 
-    para, cov = op.curve_fit(lambda t, a, b: helper(t, dt, x0, pr, a, b, p0, temp0, tempc), xdata=t, ydata=temp, p0=(0.000001, 0.08), sigma=sigma)
+    para, cov = op.curve_fit(lambda t, a, b: helper(t, dt, x0, pr, a, b, p0, temp0, tempc),
+                             xdata=t,
+                             ydata=temp,
+                             p0=(0.000001, 0.08),
+                             sigma=sigma)
 
     return para, cov
